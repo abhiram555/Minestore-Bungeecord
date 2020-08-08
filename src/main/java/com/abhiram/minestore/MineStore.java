5ad27a4ac533da91f63a05a33bf44772e77a9162
abhiram555 <abhithegamer2019@gmail.com>
2020-08-08 19:50:57 +0530

    Initial commit

---
+++ src/main/java/com/abhiram/minestore/MineStore.java
@@ -0,0 +1,55 @@
+package com.abhiram.minestore;
+
+
+import com.abhiram.minestore.websocket.CommandHandler;
+import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.config.Configuration;
+import net.md_5.bungee.config.ConfigurationProvider;
+import net.md_5.bungee.config.YamlConfiguration;
+import sun.dc.pr.PRError;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.util.concurrent.TimeUnit;
+
+public class MineStore extends Plugin {
+    private File file;
+    private Configuration config;
+
+
+    @Override
+    public void onEnable() {
+        try{
+          LoadConfig();
+          int port = config.getInt("Websocket-port");
+          getProxy().getScheduler().schedule(this,new CommandHandler(this,port),2,2, TimeUnit.MILLISECONDS);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        getLogger().info("Enabled Checking for Order.");
+    }
+
+
+
+    private void LoadConfig() throws Exception{
+
+        if (!getDataFolder().exists())
+            getDataFolder().mkdir();
+
+        file = new File(getDataFolder(), "config.yml");
+
+
+        if (!file.exists()) {
+            try (InputStream in = getResourceAsStream("config.yml")) {
+                Files.copy(in, file.toPath());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        config = ConfigurationProvider.getProvider(YamlConfiguration.class).load(file);
+    }
+}
---
+++ src/main/java/com/abhiram/minestore/websocket/CommandHandler.java
@@ -0,0 +1,43 @@
+package com.abhiram.minestore.websocket;
+
+import com.abhiram.minestore.MineStore;
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
+import net.md_5.bungee.api.config.ServerInfo;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.connection.Server;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class CommandHandler implements Runnable{
+    private ServerSocket websocket;
+    private MineStore plugin;
+
+
+    public CommandHandler(MineStore plugin,int port)throws Exception{
+        this.plugin = plugin;
+        websocket = new ServerSocket(port);
+    }
+    public void run(){
+        try{
+            Socket soc = websocket.accept();
+            BufferedReader read = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+            String content = read.readLine();
+            read.close();
+
+            final String[] pass = content.split("  ");
+
+            final ByteArrayDataOutput sendcontent = ByteStreams.newDataOutput();
+            sendcontent.writeUTF(pass[1]);
+            for(ServerInfo server : plugin.getProxy().getServers().values()){
+                server.sendData("my:minestore",sendcontent.toByteArray());
+            }
+            plugin.getLogger().info("Send Order To Minestore-Spigot/Bukkit!");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
